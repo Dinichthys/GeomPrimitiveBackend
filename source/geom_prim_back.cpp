1103e63f6e1a49f57e070914e8886641d329ea7f
Melnikov Denis <melnikov.denis.aleksandrovich@gmail.com>
2025-11-14 05:18:12 +0300

    Circle

--- source/geom_prim_back.cpp
+++ source/geom_prim_back.cpp
@@ -2,7 +2,14 @@
 #include "geom_prim.hpp"
 
 hui::GeomPrim* GeomPrimBack::CreateGeomPrim(__attribute_maybe_unused__ size_t geomPrimType, dr4::Window* dr4Window) {
-    return new Rectangle(dr4Window->CreateRectangle());
+    switch(geomPrimType) {
+        case 0:
+            return new Rectangle(dr4Window->CreateRectangle());
+        case 1:
+            return new Circle(dr4Window->CreateCircle());
+        default:
+            return NULL;
+    }
 }
 
 extern "C" hui::GeomPrimBackend* GEOM_PRIM_BACKEND_FUNCTION () {
