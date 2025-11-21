00fdc5108d35ce0b72f53656fc0252a84a896011
Melnikov Denis <melnikov.denis.aleksandrovich@gmail.com>
2025-11-21 04:37:54 +0300

    Drawing

--- source/geom_prim_tools.cpp
+++ source/geom_prim_tools.cpp
@@ -15,12 +15,20 @@ bool RectangleTool::OnMouseDown(const dr4::Event::MouseButton &evt) {
 }
 
 bool RectangleTool::OnMouseUp(const dr4::Event::MouseButton &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     rect_->OnMouseUp(evt);
     OnEnd();
     return true;
 }
 
 bool RectangleTool::OnMouseMove(const dr4::Event::MouseMove &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     rect_->OnMouseMove(evt);
     return true;
 }
@@ -40,12 +48,20 @@ bool CircleTool::OnMouseDown(const dr4::Event::MouseButton &evt) {
 }
 
 bool CircleTool::OnMouseUp(const dr4::Event::MouseButton &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     circle_->OnMouseUp(evt);
     OnEnd();
     return true;
 }
 
 bool CircleTool::OnMouseMove(const dr4::Event::MouseMove &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     circle_->OnMouseMove(evt);
     return true;
 }
@@ -64,12 +80,20 @@ bool ArrowTool::OnMouseDown(const dr4::Event::MouseButton &evt) {
 }
 
 bool ArrowTool::OnMouseUp(const dr4::Event::MouseButton &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     arrow_->OnMouseUp(evt);
     OnEnd();
     return true;
 }
 
 bool ArrowTool::OnMouseMove(const dr4::Event::MouseMove &evt) {
+    if (!is_drawing_) {
+        return false;
+    }
+
     arrow_->OnMouseMove(evt);
     return true;
 }
